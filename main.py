8709ba722486036d7997e6d8415ad0baa887a59b
Nacho Geoghegan <ignaciog00@gmail.com>
2024-03-27 12:29:53 -0300

    added utility file "main" for easy rendering

---
+++ main.py
@@ -0,0 +1,27 @@
+import subprocess
+
+
+def render_slides(scenes):
+    try:
+        # Run the manim render CLI command
+        subprocess.run(["manim", "render", "slides.py", "-p", "-ql", *scenes])
+        print("Slides rendered successfully!")
+    except FileNotFoundError:
+        print("Manim is not installed. Please install it using 'pip install manim'.")
+
+
+def generate_html_presentation(scenes, output_file):
+    try:
+        # Run the manim-slides convert CLI command
+        subprocess.run(["manim-slides", "convert", *scenes, output_file , "--open"])
+        print("HTML presentation generated successfully!")
+    except FileNotFoundError:
+        print(
+            "manim-slides is not installed. Please install it using 'pip install manim-slides'."
+        )
+
+
+if __name__ == "__main__":
+    scenes = ["Introduction", "WithTeX", "Outro"]
+    render_slides(scenes)
+    generate_html_presentation(scenes, "_site/index.html")
